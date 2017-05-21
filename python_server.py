c521603204290d216e98c883d6c133707c05bf5b
w <contact@neontiles.com>
2017-05-21 21:58:28 +0200

    first commit

--- python_server.py
+++ python_server.py
@@ -1,2 +1,5 @@
-python -m http.server 8080
+# for Python3
+python3 -m http.server 8080
 
+# for Python2
+python2 -m SimpleHTTPServer 8080
--- rates.py
+++ rates.py
@@ -3,7 +3,7 @@
 import requests
 import json
 
-r=requests.get("https://api.fixer.io/latest?base=USD&symbols=NOK")
+r=requests.get("https://api.fixer.io/latest?base=EUR&symbols=NOK")
 resp=json.loads(r.text)
 print (resp["base"], resp["rates"]["NOK"])
 
