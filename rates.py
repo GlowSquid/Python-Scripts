a9d7eb9880ea94b7cdc2ab3fdca0e208e55591b0
GlowSquid <koz@glowsquid.com>
2018-06-08 05:35:46 +0200

    api update

--- rates.py
+++ rates.py
@@ -3,9 +3,12 @@
 import requests
 import json
 
-r=requests.get("https://api.fixer.io/latest?base=EUR&symbols=NOK")
-resp=json.loads(r.text)
+# Using fixer.io
+# r = requests.get("https://api.fixer.io/latest?base=EUR&symbols=NOK") # Old way. No more HTTPS encryption or extended currencies without subscription..
+# r = requests.get("http://data.fixer.io/api/latest?access_key=<YOUR_API>&base=EUR&symbols=NOK") # New way
+
+# Using exchangeratesapi.io
+r = requests.get("https://exchangeratesapi.io/api/latest?base=EUR&symbols=NOK")
+
+resp = json.loads(r.text)
 print (resp["base"], resp["rates"]["NOK"])
-
-
-
